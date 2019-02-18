aedde2468974ca1d64940ca2537d159bd26852ae
Zack Marvel <zpmarvel@gmail.com>
2019-02-18 00:18:39 -0700

    "main"

--- src/nbt_dump.cpp
+++ src/nbt_dump.cpp
@@ -1,9 +1,35 @@
 
 
+#include <iostream>
+
 #include "nbt.hpp"
+
+
+const char *USAGE = " input_file [-o output_file]\n"
+"\n"
+"    input_file                  NBT file\n"
+"\n"
+"    -o, --output output_file    File to which dump NBT structure and\n"
+"                                contents should be dumped (default=stdout)\n";
+
 
 int main(int argc, char* argv[]) {
 
+  if (argc < 2) {
+    std::cerr << "Not enough arguments" << std::endl << argv[0] << USAGE;
+    return 1;
+  }
+
+  NBTFile input{argv[1]};
+  // Assume the first ID identifies a Compound tag
+  input.readID();
+
+  try {
+    CompoundTag root = input.readCompoundTag();
+  }
+  catch (NBTTagException& e) {
+    std::cerr << "NBTTagException: " << e.what() << std::endl;
+  }
 
   return 0;
 }
