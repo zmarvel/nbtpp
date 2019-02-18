fac0e02230dc5f491d663e291ce6486f184b788b
Zack Marvel <zpmarvel@gmail.com>
2019-02-17 23:09:55 -0700

    Add tests
    Currently they fail with a linker error :(.

---
+++ test/test_nbt.cpp
@@ -0,0 +1,165 @@
+/*
+ * Copyright (C) 2019  Zack Marvel
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "catch2/catch.hpp"
+
+#include "nbt.hpp"
+
+
+TEST_CASE("Reading from test files", "[nbtfile]") {
+  SECTION("Single, primitive-tag files") {
+    {
+      NBTFile file{"./test/data/end_tag.dat"};
+      TagID id = file.readID();
+      REQUIRE(id == TagID::END);
+      EndTag tag = file.readTag<EndTag>();
+    }
+    {
+      NBTFile file{"./test/data/byte_tag.dat"};
+      TagID id = file.readID();
+      REQUIRE(id == TagID::BYTE);
+      ByteTag tag = file.readTag<ByteTag>();
+      REQUIRE(tag.name == "byte tag");
+      REQUIRE(tag.value == 0x40);
+    }
+    {
+      NBTFile file{"./test/data/short_tag.dat"};
+      TagID id = file.readID();
+      REQUIRE(id == TagID::SHORT);
+      ShortTag tag = file.readTag<ShortTag>();
+      REQUIRE(tag.name == "short tag");
+      REQUIRE(tag.value == 0x40);
+    }
+    {
+      NBTFile file{"./test/data/int_tag.dat"};
+      TagID id = file.readID();
+      REQUIRE(id == TagID::INT);
+      IntTag tag = file.readTag<IntTag>();
+      REQUIRE(tag.name == "int tag");
+      REQUIRE(tag.value == 0x40000000);
+    }
+    {
+      NBTFile file{"./test/data/long_tag.dat"};
+      TagID id = file.readID();
+      REQUIRE(id == TagID::LONG);
+      LongTag tag = file.readTag<LongTag>();
+      REQUIRE(tag.name == "long tag");
+      REQUIRE(tag.value == 0x4000000030000000);
+    }
+    {
+      NBTFile file{"./test/data/float_tag.dat"};
+      TagID id = file.readID();
+      REQUIRE(id == TagID::FLOAT);
+      FloatTag tag = file.readTag<FloatTag>();
+      REQUIRE(tag.name == "float tag");
+      REQUIRE(tag.value == 64.0f);
+    }
+    {
+      NBTFile file{"./test/data/double_tag.dat"};
+      TagID id = file.readID();
+      REQUIRE(id == TagID::DOUBLE);
+      DoubleTag tag = file.readTag<DoubleTag>();
+      REQUIRE(tag.name == "double tag");
+      REQUIRE(tag.value == 64.0);
+    }
+  }
+
+  SECTION("Files with complex tags") {
+    {
+      NBTFile file{"./test/data/byte_array_tag.dat"};
+      TagID id = file.readID();
+      REQUIRE(id == TagID::BYTE_ARRAY);
+      ByteArrayTag tag = file.readTag<ByteArrayTag>();
+      REQUIRE(tag.name == "byte array tag");
+      std::vector<int8_t> expected{
+        0x12, 0x23, 0x34, 0x45
+      };
+      REQUIRE(*tag.value == expected);
+    }
+    {
+      NBTFile file{"./test/data/int_array_tag.dat"};
+      TagID id = file.readID();
+      REQUIRE(id == TagID::INT_ARRAY);
+      IntArrayTag tag = file.readTag<IntArrayTag>();
+      REQUIRE(tag.name == "int array tag");
+      std::vector<int32_t> expected{
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+        0x44332211, 0x55443322, 0x66554433, 0x77665544
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+        0x11223344, 0x22334455, 0x33445566, 0x44556677
+#endif
+      };
+      REQUIRE(*tag.value == expected);
+    }
+    {
+      NBTFile file{"./test/data/long_array_tag.dat"};
+      TagID id = file.readID();
+      REQUIRE(id == TagID::LONG_ARRAY);
+      LongArrayTag tag = file.readTag<LongArrayTag>();
+      REQUIRE(tag.name == "long array tag");
+      std::vector<int64_t> expected{
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+        0x0877665544332211, 0x0988776655443322,
+        0x0a99887766554433, 0x0baa998877665544
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+        0x1122334455667708, 0x2233445566778809,
+        0x334455667788990a, 0x445566778899aa0b
+#endif
+      };
+      REQUIRE(*tag.value == expected);
+    }
+    {
+      NBTFile file{"./test/data/string_tag.dat"};
+      TagID id = file.readID();
+      REQUIRE(id == TagID::STRING);
+      StringTag tag = file.readTag<StringTag>();
+      REQUIRE(tag.name == "string tag");
+      std::string expected{"The quick brown fox jumped over the lazy dog"};
+      REQUIRE(*tag.value == expected);
+    }
+    {
+      NBTFile file{"./test/data/list_byte_tag.dat"};
+      TagID id = file.readID();
+      REQUIRE(id == TagID::LIST);
+      // works
+      //ListTag<ByteTag> tag = file.readTagList<ByteTag>(TagID::LIST, "");
+      // doesn't work
+      ListTag<ByteTag> tag = file.readTagList<ByteTag>();
+      REQUIRE(tag.name == "listof byte tag");
+      std::vector<int8_t> expected{
+        0x7f, 0x6e, 0x5d, 0x4c
+      };
+      REQUIRE(*tag.value == expected);
+    }
+    //{
+    //  NBTFile file{"./test/data/list_string_tag.dat"};
+    //  TagID id = file.readID();
+    //  REQUIRE(id == TagID::LIST);
+    //  ListTag<StringTag> tag = file.readTagList<StringTag>();
+    //  REQUIRE(tag.name == "list tag");
+    //  std::vector<std::string> expected{
+    //    "Roses are red",
+    //    "Violets are blue",
+    //    "C++ is a language for me and you"
+    //  };
+    //  for (size_t i = 0; i < expected.size(); i++) {
+    //    REQUIRE(*(tag.value->at(i)) == expected.at(i));
+    //  }
+    //}
+  }
+}
---
+++ test/test_swaps.cpp
@@ -0,0 +1,274 @@
+/*
+ * Copyright (C) 2019  Zack Marvel
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "catch2/catch.hpp"
+
+#include "nbt.hpp"
+
+
+TEST_CASE("Primitive type byte-swaps", "[primitive]") {
+  SECTION("File-to-host") {
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+    REQUIRE(ByteTag::ftoh(0x7e) == 0x7e);
+    REQUIRE(ShortTag::ftoh(0x7e) == 0x7e00);
+    REQUIRE(IntTag::ftoh(0x7e) == 0x7e000000);
+    REQUIRE(LongTag::ftoh(0x7e6d5c4b) == 0x4b5c6d7e00000000);
+
+    {
+      union expected_u {
+        unsigned char raw[4];
+        uint32_t uint;
+        float flt;
+      } expected;
+      expected.raw[0] = 0x42;
+      expected.raw[1] = 0x80;
+      expected.raw[2] = 0x00;
+      expected.raw[3] = 0x00;
+
+      REQUIRE(FloatTag::ftoh(64.0f) == expected.flt);
+    }
+    {
+      union expected_u {
+        unsigned char raw[8];
+        uint64_t uint;
+        double flt;
+      } expected;
+      expected.raw[0] = 0x40;
+      expected.raw[1] = 0x50;
+      expected.raw[2] = 0x00;
+      expected.raw[3] = 0x00;
+      expected.raw[4] = 0x00;
+      expected.raw[5] = 0x00;
+      expected.raw[6] = 0x00;
+      expected.raw[7] = 0x00;
+
+      REQUIRE(DoubleTag::ftoh(64.0) == expected.flt);
+    }
+
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    // These should be no-ops
+    REQUIRE(ByteTag::ftoh(0x7e) == 0x7e);
+    REQUIRE(ShortTag::ftoh(0x7e) == 0x7e);
+    REQUIRE(IntTag::ftoh(0x7e) == 0x7e);
+    REQUIRE(LongTag::ftoh(0x7e6d5c4b) == 0x7e6d5c4b);
+
+    REQUIRE(FloatTag::ftoh(64.0f) == 64.0f);
+    REQUIRE(DoubleTag::ftoh(64.0) == 64.0);
+#endif
+  }
+  SECTION("Host-to-file") {
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+    REQUIRE(ByteTag::htof(0x7e) == 0x7e);
+    REQUIRE(ShortTag::htof(0x7e00) == 0x7e);
+    REQUIRE(IntTag::htof(0x7e000000) == 0x7e);
+    REQUIRE(LongTag::htof(0x4b5c6d7e00000000) == 0x7e6d5c4b);
+
+    { // ['0x0', '0x24', '0x74', '0x49']
+      union expected_u {
+        unsigned char raw[4];
+        uint32_t uint;
+        float flt;
+      } expected;
+      expected.raw[0] = 0x49;
+      expected.raw[1] = 0x74;
+      expected.raw[2] = 0x24;
+      expected.raw[3] = 0x00;
+
+      REQUIRE(FloatTag::htof(1e6f) == expected.flt);
+    }
+    { // ['0x0', '0x0', '0x0', '0x0', '0x65', '0xcd', '0xcd', '0x41']
+      union expected_u {
+        unsigned char raw[8];
+        uint64_t uint;
+        double flt;
+      } expected;
+      expected.raw[0] = 0x41;
+      expected.raw[1] = 0xcd;
+      expected.raw[2] = 0xcd;
+      expected.raw[3] = 0x65;
+      expected.raw[4] = 0x00;
+      expected.raw[5] = 0x00;
+      expected.raw[6] = 0x00;
+      expected.raw[7] = 0x00;
+
+      REQUIRE(DoubleTag::htof(1e9) == expected.flt);
+    }
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    // These should be no-ops
+    REQUIRE(ByteTag::htof(0x7e) == 0x7e);
+    REQUIRE(ShortTag::htof(0x7e) == 0x7e);
+    REQUIRE(IntTag::htof(0x7e) == 0x7e);
+    REQUIRE(LongTag::htof(0x7e6d5c4b) == 0x7e6d5c4b);
+
+    REQUIRE(FloatTag::htof(1e6f) == 1e6f);
+    REQUIRE(DoubleTag::htof(1e9) == 1e9);
+#endif
+  }
+}
+
+
+TEST_CASE("Array type byte-swaps", "[array]") {
+  SECTION("File-to-host") {
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+    {
+      std::vector<int8_t> vec = {
+        0x12, 0x13, 0x14, 0x15
+      };
+      std::vector<int8_t> expVec{vec};
+      std::unique_ptr<std::vector<int8_t>> upVec =
+        std::make_unique<std::vector<int8_t>>(vec);
+      std::unique_ptr<std::vector<int8_t>> upOutVec =
+        std::move(ByteArrayTag::ftoh(std::move(upVec)));
+      REQUIRE(*upOutVec == expVec);
+    }
+    {
+      std::vector<int32_t> vec = {
+        0x12, 0x13, 0x14, 0x15
+      };
+      std::vector<int32_t> expVec = {
+        0x12000000, 0x13000000, 0x14000000, 0x15000000
+      };
+      std::unique_ptr<std::vector<int32_t>> upVec =
+        std::make_unique<std::vector<int32_t>>(vec);
+      std::unique_ptr<std::vector<int32_t>> upOutVec =
+        std::move(IntArrayTag::ftoh(std::move(upVec)));
+      REQUIRE(*upOutVec == expVec);
+    }
+    {
+      std::vector<int64_t> vec = {
+        0x12, 0x13, 0x14, 0x15
+      };
+      std::vector<int64_t> expVec = {
+        0x1200000000000000, 0x1300000000000000,
+        0x1400000000000000, 0x1500000000000000
+      };
+      std::unique_ptr<std::vector<int64_t>> upVec =
+        std::make_unique<std::vector<int64_t>>(vec);
+      std::unique_ptr<std::vector<int64_t>> upOutVec =
+        std::move(LongArrayTag::ftoh(std::move(upVec)));
+      REQUIRE(*upOutVec == expVec);
+    }
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    {
+      std::vector<int8_t> vec = {
+        0x12, 0x13, 0x14, 0x15
+      };
+      std::unique_ptr<std::vector<int8_t>> upVec =
+        std::make_unique<std::vector<int8_t>>(vec);
+      std::unique_ptr<std::vector<int8_t>> upOutVec =
+        std::move(ByteArrayTag::ftoh(std::move(upVec)));
+      REQUIRE(*upOutVec == vec);
+    }
+    {
+      std::vector<int32_t> vec = {
+        0x12, 0x13, 0x14, 0x15
+      };
+      std::unique_ptr<std::vector<int32_t>> upVec =
+        std::make_unique<std::vector<int32_t>>(vec);
+      std::unique_ptr<std::vector<int32_t>> upOutVec =
+        std::move(IntArrayTag::ftoh(std::move(upVec)));
+      REQUIRE(*upOutVec == vec);
+    }
+    {
+      std::vector<int64_t> vec = {
+        0x12, 0x13, 0x14, 0x15
+      };
+      std::unique_ptr<std::vector<int64_t>> upVec =
+        std::make_unique<std::vector<int64_t>>(vec);
+      std::unique_ptr<std::vector<int64_t>> upOutVec =
+        std::move(LongArrayTag::ftoh(std::move(upVec)));
+      REQUIRE(*upOutVec == vec);
+    }
+#endif
+  }
+
+  SECTION("Host-to-file") {
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+    {
+      std::vector<int8_t> vec = {
+        0x12, 0x13, 0x14, 0x15
+      };
+      std::vector<int8_t> expVec{vec};
+      std::unique_ptr<std::vector<int8_t>> upVec =
+        std::make_unique<std::vector<int8_t>>(vec);
+      std::unique_ptr<std::vector<int8_t>> upOutVec =
+        std::move(ByteArrayTag::htof(std::move(upVec)));
+      REQUIRE(*upOutVec == expVec);
+    }
+    {
+      std::vector<int32_t> vec = {
+        0x12000000, 0x13000000, 0x14000000, 0x15000000
+      };
+      std::vector<int32_t> expVec = {
+        0x12, 0x13, 0x14, 0x15
+      };
+      std::unique_ptr<std::vector<int32_t>> upVec =
+        std::make_unique<std::vector<int32_t>>(vec);
+      std::unique_ptr<std::vector<int32_t>> upOutVec =
+        std::move(IntArrayTag::htof(std::move(upVec)));
+      REQUIRE(*upOutVec == expVec);
+    }
+    {
+      std::vector<int64_t> vec = {
+        0x1200000000000000, 0x1300000000000000,
+        0x1400000000000000, 0x1500000000000000
+      };
+      std::vector<int64_t> expVec = {
+        0x12, 0x13, 0x14, 0x15
+      };
+      std::unique_ptr<std::vector<int64_t>> upVec =
+        std::make_unique<std::vector<int64_t>>(vec);
+      std::unique_ptr<std::vector<int64_t>> upOutVec =
+        std::move(LongArrayTag::htof(std::move(upVec)));
+      REQUIRE(*upOutVec == expVec);
+    }
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    {
+      std::vector<int8_t> vec = {
+        0x12, 0x13, 0x14, 0x15
+      };
+      std::unique_ptr<std::vector<int8_t>> upVec =
+        std::make_unique<std::vector<int8_t>>(vec);
+      std::unique_ptr<std::vector<int8_t>> upOutVec =
+        std::move(ByteArrayTag::htof(std::move(upVec)));
+      REQUIRE(*upOutVec == vec);
+    }
+    {
+      std::vector<int32_t> vec = {
+        0x12000000, 0x13000000, 0x14000000, 0x15000000
+      };
+      std::unique_ptr<std::vector<int32_t>> upVec =
+        std::make_unique<std::vector<int32_t>>(vec);
+      std::unique_ptr<std::vector<int32_t>> upOutVec =
+        std::move(IntArrayTag::htof(std::move(upVec)));
+      REQUIRE(*upOutVec == vec);
+    }
+    {
+      std::vector<int64_t> vec = {
+        0x1200000000000000, 0x1300000000000000,
+        0x1400000000000000, 0x1500000000000000
+      };
+      std::unique_ptr<std::vector<int64_t>> upVec =
+        std::make_unique<std::vector<int64_t>>(vec);
+      std::unique_ptr<std::vector<int64_t>> upOutVec =
+        std::move(LongArrayTag::htof(std::move(upVec)));
+      REQUIRE(*upOutVec == vec);
+    }
+#endif
+  }
+}
