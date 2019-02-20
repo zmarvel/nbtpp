d7f9cec4ed7add3cf82ca8b1290f373e27c8ede4
Zack Marvel <zpmarvel@gmail.com>
2019-02-19 23:12:59 -0700

    silly script to help encode data

---
+++ test/data/encode.py
@@ -0,0 +1,38 @@
+
+
+import struct
+
+def print_hex(seq):
+    print(' '.join('{:x}'.format(c) for c in bytes(seq)))
+
+def enc_str(s):
+    print(len(s))
+    print_hex(s)
+
+def enc_double(d):
+    print_hex(struct.pack('>d', d))
+
+def enc_float(f):
+    print_hex(struct.pack('>f', f))
+
+
+if __name__ == '__main__':
+    import argparse as ap
+
+    parser = ap.ArgumentParser()
+    parser.add_argument('-s', '--string', action='store_true',
+                        help='Encode the argument as a string')
+    parser.add_argument('-f', '--float', action='store_true',
+                        help='Encode the argument as a float')
+    parser.add_argument('-d', '--double', action='store_true',
+                        help='Encode the argument as a double')
+    parser.add_argument('value',
+                        help='Unencoded value')
+    args = parser.parse_args()
+
+    if args.string:
+        enc_str(str(args.value))
+    elif args.float:
+        enc_float(float(args.value))
+    elif args.double:
+        enc_double(float(args.value))
